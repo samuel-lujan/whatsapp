dbabe10a42dd14b2961c239cffb502020e985f69
devdeca <andrebarreto.dev@gmail.com>
2026-03-15 19:31:18 -0300

    remove unused workaround

--- app/wppwebjs/session/client.ts
+++ app/wppwebjs/session/client.ts
@@ -66,11 +66,6 @@ export async function createSession(companySlug: string): Promise<void> {
         "--disable-component-update",
       ],
     },
-    webVersionCache: {
-      type: "remote",
-      remotePath:
-        "https://raw.githubusercontent.com/wppconnect-team/wa-version/refs/heads/main/html/2.3000.1033090211-alpha.html",
-    },
   });
 
   sessions[companySlug] = {
